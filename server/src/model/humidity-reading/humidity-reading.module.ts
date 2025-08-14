1b76a8c20bbd6619ed0b612788a365bd5d0a019a
NotHydra <rizky.irswanda115@gmail.com>
2025-08-14 11:06:28 +0800

    feat(server): add HumidityReadingModule and HumidityReadingService for managing humidity readings

---
+++ server/src/model/humidity-reading/humidity-reading.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+
+import { ConfigService } from "@/config/config.service";
+
+import { PrismaService } from "@/provider/prisma.service";
+import { UtilityService } from "@/provider/utility.service";
+
+import { IoTDeviceService } from "@/model/iot-device/iot-device.service";
+
+import { HumidityReadingService } from "./humidity-reading.service";
+
+@Module({
+    controllers: [],
+    providers: [ConfigService, UtilityService, PrismaService, IoTDeviceService, HumidityReadingService],
+    imports: [],
+    exports: [HumidityReadingService],
+})
+export class HumidityReadingModule {}
