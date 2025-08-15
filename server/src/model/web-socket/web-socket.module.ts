e6def6ef3dd71c23f27de255bfadcb25a236d30a
NotHydra <rizky.irswanda115@gmail.com>
2025-08-16 01:23:40 +0800

    feat(server): enhance WebSocket service with IoT device topic subscription and improved error handling

--- server/src/model/web-socket/web-socket.module.ts
+++ server/src/model/web-socket/web-socket.module.ts
@@ -2,14 +2,17 @@ import { Module } from "@nestjs/common";
 
 import { ConfigService } from "@/config/config.service";
 
+import { PrismaService } from "@/provider/prisma.service";
 import { UtilityService } from "@/provider/utility.service";
+
+import { IoTDeviceService } from "@/model/iot-device/iot-device.service";
 
 import { WebSocketController } from "./web-socket.controller";
 import { WebSocketService } from "./web-socket.service";
 
 @Module({
     controllers: [WebSocketController],
-    providers: [ConfigService, UtilityService, WebSocketService],
+    providers: [ConfigService, UtilityService, PrismaService, IoTDeviceService, WebSocketService],
     imports: [],
     exports: [],
 })
